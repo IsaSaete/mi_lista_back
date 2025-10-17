aed26193b5dd3a6049addaa62d201cb381d5f0af
Isabel Sáenz <isasaete@gmail.com>
2025-10-17 12:29:43 +0200

    Add handleErrors middleware to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import checkHealthStatus from "./middleware/checkHealthStatus/checkHealthStatus.js";
+import handleErrors from "./middleware/hanldeErrors/handleErrors.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.get("/", checkHealthStatus);
+
+app.use(handleErrors);
 
 export default app;
