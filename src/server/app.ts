8809a2edd15a0398cda6fe7ee06d75164df6862d
Isabel Sáenz <isasaete@gmail.com>
2025-10-20 19:33:23 +0200

    Add handleCors middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import checkHealthStatus from "./middleware/checkHealthStatus/checkHealthStatus.js";
 import handleErrors from "./middleware/hanldeErrors/handleErrors.js";
 import shoppingListRouter from "../menu/router/shoppingListRouter.js";
-
 import handleEndpointNotFound from "./middleware/handleEndpointNotFound/handleEndpointNotFound.js";
+import handleCors from "./middleware/handleCors/handleCors.js";
 
 const app = express();
 
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+app.use(handleCors);
 
 app.use(express.json());
 
