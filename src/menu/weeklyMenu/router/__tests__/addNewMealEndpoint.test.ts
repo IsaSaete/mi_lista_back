11e84b0f1a93eb80c679cb4ffadbd5ea10f6107d
Isabel Sáenz <isasaete@gmail.com>
2025-11-04 20:14:58 +0100

    Add test of POST/weekly-menu endpoint

---
+++ src/menu/weeklyMenu/router/__tests__/addNewMealEndpoint.test.ts
@@ -0,0 +1,39 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import connectToDatabase from "../../../../database/connectToDatabase.js";
+import app from "../../../../server/app.js";
+import { tuesdayLunch } from "../../fixtures/fixtures.js";
+import { NewMealBodyResponse } from "../../controller/types.js";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+
+  const mongoDbConnectionString = server.getUri();
+
+  await connectToDatabase(mongoDbConnectionString);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await server.stop();
+});
+
+describe("Given the POST /weekly-menu endpoint", () => {
+  describe("When it receives a request with tuesday lunch data", () => {
+    test("Then it should respond wit a 201 status code ant Tuesday lunch meal", async () => {
+      const expectedStatus = 201;
+
+      const response = await request(app)
+        .post("/weekly-menu")
+        .send(tuesdayLunch);
+
+      const body = (await response.body) as NewMealBodyResponse;
+
+      expect(response.status).toBe(expectedStatus);
+      expect(body.weeklyMenu.M).toEqual(tuesdayLunch.M);
+    });
+  });
+});
