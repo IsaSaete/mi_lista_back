3dc197cdc24f02eed6a68125c22a4c7a7fad4b3d
Isabel Sáenz <isasaete@gmail.com>
2025-11-10 12:19:07 +0100

    Modify getWeeklyMenuData and eliminate query params

--- src/menu/weeklyMenu/controller/__tests__/getWeeklyMenu.test.ts
+++ src/menu/weeklyMenu/controller/__tests__/getWeeklyMenu.test.ts
@@ -1,15 +1,9 @@
 import { Request, Response } from "express";
 import { Model } from "mongoose";
 import { WeeklyMenuStructure } from "../../types.js";
-import {
-  monday,
-  mondayEmpty,
-  tuesday,
-  weeklyMenu,
-  weeklyMenuEmpty,
-} from "../../fixtures/fixtures.js";
+import { weeklyMenu, weeklyMenuEmpty } from "../../fixtures/fixtures.js";
 import WeeklyMenuController from "../WeeklyMenuController.js";
-import { WeeklyMenuRequest, WeeklyMenuResponse } from "../types.js";
+import { WeeklyMenuResponse } from "../types.js";
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -21,8 +15,8 @@ describe("Given the getWeeklyMenu method of weeklyMenuController", () => {
     json: jest.fn(),
   };
 
-  describe("When it receives a request for an existing menu with day 'M'", () => {
-    const req: Pick<Request, "query"> = { query: { day: "M" } };
+  describe("When it receives a request", () => {
+    const req = {} as Request;
 
     const weeklyMenuModel: Pick<Model<WeeklyMenuStructure>, "findOne"> = {
       findOne: jest.fn().mockResolvedValue(weeklyMenu),
@@ -35,51 +29,24 @@ describe("Given the getWeeklyMenu method of weeklyMenuController", () => {
         weeklyMenuModel as Model<WeeklyMenuStructure>,
       );
 
-      await weeklyMenuController.getWeeklyMenu(
-        req as WeeklyMenuRequest,
-        res as WeeklyMenuResponse,
-      );
+      await weeklyMenuController.getWeeklyMenu(req, res as WeeklyMenuResponse);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
     });
 
-    test("Then it should call the response's method json with Tuesday's menu data", async () => {
+    test("Then it should call the response's method json with menu data", async () => {
       const weeklyMenuController = new WeeklyMenuController(
         weeklyMenuModel as Model<WeeklyMenuStructure>,
       );
 
-      await weeklyMenuController.getWeeklyMenu(
-        req as WeeklyMenuRequest,
-        res as WeeklyMenuResponse,
-      );
+      await weeklyMenuController.getWeeklyMenu(req, res as WeeklyMenuResponse);
 
-      expect(res.json).toHaveBeenCalledWith(tuesday);
-    });
-  });
-
-  describe("When it receives a request without day query parameter", () => {
-    const req: Pick<Request, "query"> = { query: { day: "" } };
-
-    const weeklyMenuModel: Pick<Model<WeeklyMenuStructure>, "findOne"> = {
-      findOne: jest.fn().mockResolvedValue(weeklyMenu),
-    };
-
-    test("Then it should call the response's method json with the default Monday's menu data, 'L'", async () => {
-      const weeklyMenuController = new WeeklyMenuController(
-        weeklyMenuModel as Model<WeeklyMenuStructure>,
-      );
-
-      await weeklyMenuController.getWeeklyMenu(
-        req as WeeklyMenuRequest,
-        res as WeeklyMenuResponse,
-      );
-
-      expect(res.json).toHaveBeenCalledWith(monday);
+      expect(res.json).toHaveBeenCalledWith(weeklyMenu.weeklyMenu);
     });
   });
 
   describe("When it receives the first request and no menu exists in database", () => {
-    const req: Pick<Request, "query"> = { query: { day: "" } };
+    const req = {} as Request;
 
     const weeklyMenuModel: Pick<
       Model<WeeklyMenuStructure>,
@@ -94,25 +61,19 @@ describe("Given the getWeeklyMenu method of weeklyMenuController", () => {
         weeklyMenuModel as Model<WeeklyMenuStructure>,
       );
 
-      await weeklyMenuController.getWeeklyMenu(
-        req as WeeklyMenuRequest,
-        res as WeeklyMenuResponse,
-      );
+      await weeklyMenuController.getWeeklyMenu(req, res as WeeklyMenuResponse);
 
       expect(res.status).toHaveBeenCalledWith(200);
     });
 
-    test("Then it should call the response's method json with a empty menu structure for Monday's", async () => {
+    test("Then it should call the response's method json with a empty menu", async () => {
       const weeklyMenuController = new WeeklyMenuController(
         weeklyMenuModel as Model<WeeklyMenuStructure>,
       );
 
-      await weeklyMenuController.getWeeklyMenu(
-        req as WeeklyMenuRequest,
-        res as WeeklyMenuResponse,
-      );
+      await weeklyMenuController.getWeeklyMenu(req, res as WeeklyMenuResponse);
 
-      expect(res.json).toHaveBeenCalledWith(mondayEmpty);
+      expect(res.json).toHaveBeenCalledWith(weeklyMenuEmpty.weeklyMenu);
     });
   });
 });
